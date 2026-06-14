3152768c0dd717e3429895b9eea038aa1149a9d1
Sakshi <sakshidk1502@gmail.com>
2026-06-14 13:42:58 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ await connectDB();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // API Endpoints
 app.use('/api/menu', menuRoutes);
